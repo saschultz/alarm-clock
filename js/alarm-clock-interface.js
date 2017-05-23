02af86bc7ddb832863c73e1cef946a0e0a974ea1
Aubrey Masten and Sara Schultz <aubrey@>
2017-05-23 11:40:43 -0700

    add alarms and notification

--- js/alarm-clock-interface.js
+++ js/alarm-clock-interface.js
@@ -4,8 +4,23 @@ var updateClock = function(){
   $("#clock h2").text(`${moment().format('LTS')}`);
 }
 
+var addAlarmToList = function(time){
+  $('#alarm-list ul').append(`<li>${time}</li>`);
+}
+
+var showAlert = function(){
+  $('#alarm').show();
+}
+
 $(document).ready(function(){
   $("#clock").append(`<h2>${moment().format("LTS")}</h2>`)
   var clock = new Clock();
-  clock.start(updateClock);
+  clock.start(updateClock, showAlert);
+
+  $('#alarm-input').submit(function(event) {
+    event.preventDefault();
+    var userAlarm = $('#time').val();
+    addAlarmToList(userAlarm);
+    clock.addAlarm(userAlarm);
+  });
 });
--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -1,7 +1,24 @@
-function Clock(){}
+function Clock(){
+  this.alarms = [];
+}
 
-Clock.prototype.start = function(updateClock){
-  window.setInterval(updateClock, 1000);
+Clock.prototype.start = function(updateClock, showAlert) {
+  window.setInterval(this.update, 1000, updateClock, this.anyAlarms, this.alarms, showAlert);
+}
+
+Clock.prototype.update = function(updateClock, anyAlarms, alarms, showAlert) {
+  updateClock();
+  if(anyAlarms(alarms)) {
+    showAlert();
+  }
+}
+
+Clock.prototype.anyAlarms = function(alarms){
+  return alarms.includes(moment().format('LTS'));
+}
+
+Clock.prototype.addAlarm = function(userAlarm) {
+  this.alarms.push(userAlarm.replace(/\s/, ":00 "));
 }
 
 exports.clockModule = Clock;
